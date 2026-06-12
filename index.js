10dfc9316f6ee84f35d0558c478493cf64a5627f
datcutoa <nhuyentandat@gmail.com>
2026-06-12 18:40:25 +0700

    version 2.0

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let task = [];
-task.push({
-    name:'learn english',
-    status:'pending'
-
-});
+const task = JSON.parse(localStorage.getItem('tasks')) || [];
+if(task.length === 0){
+    task.push({
+        name:'learn english',
+        status:'pending'
+    });
+    saveTask();
+}
+function saveTask(){
+localStorage.setItem('tasks',JSON.stringify(task));
+}
 let addbtn = document.getElementById('add-btn');
 function renderTask(){
     const table= document.getElementById('taskTable');
@@ -22,11 +27,47 @@ function renderTask(){
 
     })
 }
+
+ let countdown;
+function showNotification(types,title,message){
+    const notification = document.getElementById('notification');   
+    const notificationMessage = document.getElementById('notification-message');
+    const contentNotification = document.getElementById('content-notification');
+    const notificationTimer = document.getElementById('notification-timer');
+    let timer = 5;
+    notificationMessage.textContent = message;
+    notification.style.display = 'block';
+    notificationTimer.textContent = `Notification will close in ${timer} seconds`;
+   
+  
+    contentNotification.textContent = title;
+
+    if(types === 'success'){
+        notification.style.border = '3px solid green';
+    } else if(types === 'error'){
+        notification.style.border = '3px solid red';
+    }
+
+    clearInterval(countdown);
+    countdown = setInterval(()=>{
+    timer--;
+    notificationTimer.textContent = `Notification will close in ${timer} seconds`;
+    if(timer <= 0){
+        clearInterval(countdown);
+        notification.style.display = 'none';
+    }    
+
+   },1000);
+}
 function addTask(){
     let input = document.getElementById('todo-input');
     let taskName = input.value.trim();
     if (taskName ===    '') {
-        alert('Please enter a task name    ');
+       showNotification(
+        'error',
+        'Error',
+        'Task name cannot be empty'
+    );
         return;
 
     }
@@ -34,11 +75,24 @@ function addTask(){
        name:taskName,
        status:'pending'
     });
+    showNotification(
+        'success',
+        'Success',
+        'Task added successfully'
+    );
+    saveTask();
     renderTask();
     input.value = '';
 }
 function deleteTask(index){
+
     task.splice(index,1);
+    saveTask();
+  showNotification(
+    'success',
+    'Success',
+    'Task status deleted  successfully'
+);
     renderTask();
 }
 function changeTask(index){
@@ -47,6 +101,26 @@ function changeTask(index){
     } else {
         task[index].status = 'pending';
     }
+  showNotification(
+    'success',
+    'Success',
+    'Task status updated successfully'
+);
+    saveTask();
     renderTask();
 }
 renderTask();
+
+
+
+const btnAdd = document.getElementById('add-btn');
+btnAdd.addEventListener('click', addTask);
+const btnClose= document.getElementById('close-notification');
+btnClose.addEventListener('click',()=>{
+    const notification = document.getElementById('notification');
+    notification.style.display = 'none';
+})
+
+     
+
+
